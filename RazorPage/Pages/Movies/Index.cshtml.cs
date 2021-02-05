80a02c1df7fb70505bf44c6b24a021c64b379f86
mayr0y <alexlzrw@gmail.com>
2021-02-05 18:48:05 +0300

    added searching movies by name

--- RazorPage/Pages/Movies/Index.cshtml.cs
+++ RazorPage/Pages/Movies/Index.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using RazorPage.Data.Data;
 using RazorPage.Models;
@@ -21,9 +22,19 @@ namespace RazorPage.Pages.Movies
 
         public IList<Movie> Movie { get;set; }
 
-        public async Task OnGetAsync()
-        {
-            Movie = await _context.Movie.ToListAsync();
+        [BindProperty(SupportsGet =true)]
+        public string SearchString { get; set; }
+        public SelectList Genre { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string MovieGenre { get; set; }
+
+        public async Task OnGetAsync() {
+            var movies = from m in _context.Movie select m;
+            if (!string.IsNullOrEmpty(SearchString)) {
+                movies = movies.Where(s => s.Title.Contains(SearchString));
+            }
+
+            Movie = await movies.ToListAsync();
         }
     }
 }
